ee280b0617c969c5ed47782f65fc3756c4d75aeb
dshuhler <dshuhler@soundexchange.com>
2019-05-17 11:22:31 -0400

    initial commit

---
+++ src/test/java/com/shuhler/graphqlhack/GraphqlhackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shuhler.graphqlhack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GraphqlhackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
